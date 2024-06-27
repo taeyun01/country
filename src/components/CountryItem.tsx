cd0a20559aefa0b4f46ccad73bed6954204d791b
taeyun01 <lwe12f675@gmail.com>
2024-06-28 02:05:15 +0900

    feat: React제거

--- src/components/CountryItem.tsx
+++ src/components/CountryItem.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { CountryWithIsDone } from "../types/countryType";
 
 interface CountryProps {
--- src/components/CountryList.tsx
+++ src/components/CountryList.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { getCountry } from "../api/api";
 import { CountryWithIsDone } from "../types/countryType";
 import CountryItem from "./CountryItem";
